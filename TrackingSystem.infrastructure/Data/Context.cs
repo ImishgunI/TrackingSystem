db2f1da20b759ab9e3e851c43f5055eb5a73514e
michael <mvasilyev04@gmail.com>
2025-10-04 21:12:38 +0300

    Директория для взаимодействия с базой данных

---
+++ TrackingSystem.infrastructure/Data/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TrackingSystem.infrastructure.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) 
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
